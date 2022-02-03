73d24f859e0ea9b2df59e021ec9cf4ad5d50bcea
xSkinny <98687313+xSkinny@users.noreply.github.com>
2022-02-03 13:27:14 -0500

    Delete Drop Tool.lua

--- Drop Tool.lua
+++
@@ -1,7 +0,0 @@
-wait() do
-    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-        if v.name == "potion" then
-            v.Parent = game.Players.LocalPlayer.Character
-        end
-    end
-end
